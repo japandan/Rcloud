8ce62d2bc20d24839b8894fd386cbcabf3fe2eb9
Dan Vogel <dan.vogel@gmail.com>
2020-01-28 13:50:12 -0500

    Update cm_inch.R
    Added comments regarding the assignment requirements

--- cm_inch.R
+++ cm_inch.R
@@ -1,3 +1,10 @@
+# BIFX551 Assignment 1/28/2020 
+# Author: Daniel Vogel
+# Write your own function to convert between any two units. 
+# The function should take two arguments, a number, and an argument
+# specifying the direction of the conversion. 
+# Include your function with documentation string in an R-markdown file 
+#
 # function to convert between cm and inch
 # Usage: cm_inch( length, direction) where direction={ "tocm" or "toinch"}
 #
